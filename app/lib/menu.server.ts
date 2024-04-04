b728a9913544d79ce8f14c1b4966a761fa57abc0
Brooks Lybrand <brookslybrand@gmail.com>
2024-04-03 20:03:38 -0500

    Loading fake menu data

---
+++ app/lib/menu.server.ts
@@ -0,0 +1,41 @@
+"use strict";
+import { faker } from "@faker-js/faker";
+import fs from "fs/promises";
+
+export async function generateMenuItems(numItems: number) {
+  const menuItems = [];
+
+  for (let i = 0; i < numItems; i++) {
+    const menuItem = {
+      id: faker.database.mongodbObjectId(),
+      image: faker.image.urlLoremFlickr({
+        width: 300,
+        height: 300,
+        category: "food,sandwhich,salad",
+      }),
+      name: faker.lorem.words(),
+      price: faker.commerce.price({
+        min: 5,
+        max: 25,
+        symbol: "$",
+      }),
+      category: faker.helpers.arrayElement(["appetizer", "entree", "dessert"]),
+    };
+    menuItems.push(menuItem);
+  }
+
+  return menuItems;
+}
+
+async function main() {
+  try {
+    const numItems = 12; // Change this to the desired number of menu items
+    const menuItems = await generateMenuItems(numItems);
+    const fileName = "menu.json";
+    await fs.writeFile(`app/${fileName}`, JSON.stringify(menuItems, null, 2));
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+main();
--- app/routes/menu.tsx
+++ app/routes/menu.tsx
@@ -1,3 +1,24 @@
+import { useLoaderData } from "@remix-run/react";
+import { generateMenuItems } from "~/lib/menu.server";
+
+export async function loader() {
+  const menu = await generateMenuItems(12);
+  return { menu };
+}
+
 export default function Menu() {
-  return <h1>this is the menu</h1>;
+  const { menu } = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <h1>this is the menu</h1>
+      <ul>
+        {menu.map((item) => (
+          <li key={item.id}>
+            {item.name} {item.category} {item.image} {item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
