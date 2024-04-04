c9e8a12895703abd4a649b1aaf9321a8d0b6e170
Brooks Lybrand <brookslybrand@gmail.com>
2024-04-03 20:51:19 -0500

    Remove faker

--- app/lib/menu.server.ts
+++ app/lib/menu.server.ts
@@ -1,5 +1,4 @@
-"use strict";
-import { faker } from "@faker-js/faker";
+import menuJson from "./menu.json";
 
 export type MenuItem = {
   id: string;
@@ -9,36 +8,6 @@ export type MenuItem = {
   category: string;
 };
 
-export async function generateMenuItems(numItems: number): Promise<MenuItem[]> {
-  const menuItems = [];
-
-  for (let i = 0; i < numItems; i++) {
-    const menuItem = {
-      id: faker.database.mongodbObjectId(),
-      image: faker.image.urlLoremFlickr({
-        width: 300,
-        height: 300,
-        category: "food,sandwhich,salad",
-      }),
-      name: faker.lorem.words(),
-      price: faker.commerce.price({
-        min: 5,
-        max: 25,
-        symbol: "$",
-      }),
-      category: faker.helpers.arrayElement(["appetizer", "entree", "dessert"]),
-    };
-    menuItems.push(menuItem);
-  }
-
-  return menuItems;
-}
-
-let _menuItems: null | ReturnType<typeof generateMenuItems> = null;
-
 export function getMenuItems() {
-  if (!_menuItems) {
-    _menuItems = generateMenuItems(12);
-  }
-  return _menuItems;
+  return menuJson;
 }
